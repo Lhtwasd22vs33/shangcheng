231c4fc9b5cb66557de9e10c0150724cb0bd0e6d
Lhtwasd22vs33 <1438574779@qq.com>
2020-10-17 11:46:50 +0800

    10/17

--- js/sarts.js
+++ js/sarts.js
@@ -2,27 +2,32 @@
 function getid(id){
     return document.getElementById(id)
 }
+
+var cka=getid('ckall')
+
+var totalprice=getid('totalprice')
+
 var paybtn=getid('pay')
 
 var box=getid('box')
 
 var goodslist=[
-    {id:'gds1',name:'C#编程基础',price:78,num1:1},
-    {id:'gds2',name:'计算机基础',price:988,num1:2},
-    {id:'gds3',name:'photoshop',price:688,num1:8},
-    {id:'gds4',name:'web基础',price:898,num1:3},
-    {id:'gds5',name:'h5+c3',price:9,num1:18},
-    {id:'gds6',name:'javascript',price:8,num1:21},
+    {id:'gds1',name:'C#编程基础',price:78,num1:1,check:true},
+    {id:'gds2',name:'计算机基础',price:988,num1:2,check:false},
+    {id:'gds3',name:'photoshop',price:688,num1:8,check:false},
+    {id:'gds4',name:'web基础',price:898,num1:3,check:false},
+    {id:'gds5',name:'h5+c3',price:9,num1:18,check:false},
+    {id:'gds6',name:'javascript',price:8,num1:21,check:false},
 ]
 
 var s=''
 
-function co(goodslist){
+function coeatecards(goodslist){
     for(let i=0;i<goodslist.length;i++){
         s+=`<div class="cardsitem" id="">
         <div class="cds-tit">
             <label for="gds1">
-                <input type="checkbox" id="gds1">
+                <input type="checkbox" class="ck" ${goodslist[i].check ?'checked':''} data=${goodslist[i].id}>
                 ${goodslist[i].name}
             </label>
         </div>
@@ -52,5 +57,67 @@ function co(goodslist){
     
     box.innerHTML=s
 }
+countprice()
 
-co(goodslist)
+coeatecards(goodslist)
+
+var cks= document.getElementsByClassName('ck')
+console.log(cks)
+
+var flag=true
+
+for(let i=0;i<cks.length;i++){
+    cks[i].addEventListener('change',function(){
+        console.log(this)
+        console.log(this.attributes.data.value)
+        console.log(this.checked)
+        for(let j=0;j<goodslist.length;j++){
+
+            if(goodslist[j].id==this.attributes.data.value){
+                goodslist[j].check=this.checked
+                flag=this.checked
+            }
+            if(goodslist[j].check==false){
+                flag=false
+                cka.checked=false
+            }
+            if(flag==true){
+                cka.checked=true
+            }
+            countprice()
+        }
+    })
+}
+
+cka.addEventListener('change',function(){
+    console.log(this.checked)
+    if(this.checked){
+        for(let i=0;i<cks.length;i++){
+            cks[i].checked=true
+        }
+        for(let i=0;i<goodslist.length;i++){
+            goodslist[i].check=true
+        }
+        console.log(goodslist)
+    }else{
+        for(let i=0;i<cks.length;i++){
+            cks[i].checked=false
+        }
+        for(let i=0;i<goodslist.length;i++){
+            goodslist[i].check=false
+        }
+        console.log(goodslist)
+    }
+    countprice()
+})
+
+function countprice(){
+    var n=0
+
+    for(let i=0;i<goodslist.length;i++){
+        if(goodslist[i].check){
+            n+=goodslist[i].price*goodslist[i].num1
+        }
+    }
+    totalprice.innerText='总计 ： ￥'+n
+}
